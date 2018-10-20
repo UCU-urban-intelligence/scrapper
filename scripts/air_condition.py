a858fb3e26ffc594c3348160967f627f8cfd129b
IvanProdaiko94 <prodaiko94@gmail.com>
2018-10-20 15:43:56 +0300

    Change api to breezometer due to absence of data in others

--- scripts/air_condition.py
+++ scripts/air_condition.py
@@ -5,27 +5,29 @@ import requests
 class AirCondition:
     """
         For more info about API go here:
-        https://aqicn.org/json-api/doc/#api-Geolocalized_Feed
-        
-        Test data:
-        https://wiki.openstreetmap.org/wiki/Bounding_Box
-        "boundingbox":["51.2867602","51.6918741","-0.5103751","0.3340155"]
+        https://breezometer.com/air-quality-api/
     """
 
-    API_PATH = "https://api.waqi.info/feed/"
-    PRIVATE_API_TOKEN = "a480e8757e5d667d81053f4a143146f32b9ed8b2"
+    API_PATH = "https://api.breezometer.com/baqi/"
+
+    API_KEY = "194734dfed444b2b97e397498e434a85"
 
     def __init__(self):
         pass
 
     def get_df(self, lat1, lng1, lat2, lng2):
-        print(lat1, lat2, lng1, lng2)
-        mean_lat = (lat1 + lng1) / 2
-        mean_lng = (lat2 + lng2) / 2
+        print(lat1, lng1, lat2, lng2)
+        mean_lat = (lat1 + lat2) / 2
+        mean_lng = (lng1 + lng2) / 2
         print({"mean_lat": mean_lat, "mean_lng": mean_lng})
+
         response = requests.get(
-            AirCondition.API_PATH + "/geo:{};{}/".format(mean_lat, mean_lng),
-            [("token", AirCondition.PRIVATE_API_TOKEN)]
+            AirCondition.API_PATH,
+            [
+                ("lat", "{:.3f}".format(round(mean_lat, 3))),
+                ("lon", "{:.3f}".format(round(mean_lng, 3))),
+                ("key", AirCondition.API_KEY)
+            ]
         )
         return response.text
 

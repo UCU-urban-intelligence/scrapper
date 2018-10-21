466f398db08d8204fb887b8a35fe4deb6261a04d
Dima Glushko <dima2492@gmail.com>
2018-10-21 13:38:26 +0300

    Modified processing api

--- services/building_service.py
+++ services/building_service.py
@@ -1,13 +1,8 @@
-from collections import namedtuple
-
 from flask_pymongo import PyMongo
 from flask_pymongo.wrappers import Collection
-from scripts.buildings import DfCreator
+from scripts.buildings import BuildingsGetter
 from shapely import geometry
-import json
-
-
-Building = namedtuple('Building', ['addr_housenumber', 'addr_street', 'amenity', 'building', 'description', 'geometry', 'height', 'name', 'roof_shape'])
+from shapely.geometry import Point
 
 
 class BuildingService:
@@ -18,40 +13,44 @@ class BuildingService:
         self.__request_areas: Collection = mongo.db[self.__REQUEST_AREAS_COLLECTION_NAME]
         self.__buildings: Collection = mongo.db[self.__BUILDINGS_COLLECTION_NAME]
 
-    def __get_existing_bounds(self, bottom_left, top_right):
+    def __get_existing_bounds(self, bottom_left: Point, top_right: Point):
         for request_area in self.__request_areas.find():
-            if (request_area.bottom_left.lat <= bottom_left[0] <= request_area.top_right.lat and
-                    request_area.bottom_left.lng <= bottom_left[1] <= request_area.top_right.lng and
-                    request_area.bottom_left.lat <= top_right[0] <= request_area.top_right.lat and
-                    request_area.bottom_left.lng <= top_right[1] <= request_area.top_right.lng):
+            if (request_area.bottom_left.lat <= bottom_left.x <= request_area.top_right.lat and
+                    request_area.bottom_left.lng <= bottom_left.y <= request_area.top_right.lng and
+                    request_area.bottom_left.lat <= top_right.x <= request_area.top_right.lat and
+                    request_area.bottom_left.lng <= top_right.y <= request_area.top_right.lng):
                 return request_area
 
         return None
 
+
     def __save_buildings(self, buildings):
-        buildings['geometry'] = buildings['geometry'].apply(geometry.mapping)
         result = self.__buildings.insert_many(buildings.to_dict('records'))
+        return result
 
 
-        #self.__buildings.insert()
-        #self.__request_areas.insert()
-        return buildings
+    def __save_request_area(self, bottom_left: Point, top_right: Point):
+        dict = {"bottom_left" : geometry.mapping(bottom_left), "top_right" : geometry.mapping(top_right) }
+        result = self.__request_areas.insert(dict)
+        return result
 
 
-    def __prepare_buildings(self, bottom_left, top_right):
-        building_df_creator = DfCreator()
-        buildings = building_df_creator.get_df(bottom_left[0], bottom_left[1], top_right[0], top_right[1])
+    def __prepare_buildings(self, bottom_left: Point, top_right: Point):
+        building_df_creator = BuildingsGetter()
+        buildings = building_df_creator.get_df(bottom_left.x, bottom_left.y, top_right.x, top_right.y)
+        buildings['geometry'] = buildings['geometry'].apply(geometry.mapping)
         return buildings
 
 
     def get_buildings(self, bounds):
-        bottom_left = [bounds[0], bounds[1]]
-        top_right = [bounds[2], bounds[3]]
+        bottom_left = Point(bounds[0], bounds[1])
+        top_right = Point(bounds[2], bounds[3])
 
         existing_bounds = self.__get_existing_bounds(bottom_left, top_right)
         if existing_bounds:
-            a = 5
+            return None
         else:
             buildings = self.__prepare_buildings(bottom_left, top_right)
             self.__save_buildings(buildings)
-        return None
+            self.__save_request_area(bottom_left, top_right)
+            return buildings

1e394f09435bd892a30f3c55627910819118b09e
IvanProdaiko94 <prodaiko94@gmail.com>
2018-10-30 21:37:44 +0200

    basic config for getting result for bbox

--- front/src/javascripts/app.js
+++ front/src/javascripts/app.js
@@ -6,6 +6,36 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 
 var areaSelect = L.areaSelect({
   width:100,
   height:150,
-  keepAspectRatio:true
+  keepAspectRatio:false
 });
+console.log(areaSelect);
 areaSelect.addTo(map);
+
+function showLoader() {
+
+}
+
+function hideLoader() {
+
+}
+
+function fetchBBoxData(lng1, lat1, lng2, lat2) {
+  return fetch("http://localhost:8000/processing", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "bbox": [lng1, lat1, lng2, lat2]
+    })
+  });
+}
+
+document.getElementById('detect').addEventListener('click', function () {
+  const bbox = areaSelect.getBounds();
+  showLoader();
+  fetchBBoxData(bbox.getWest(), bbox.getSouth(), bbox.getEast(), bbox.getNorth())
+    .catch(err => console.error(err))
+    .then(hideLoader)
+});
